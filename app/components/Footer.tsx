eee06704741cb60ba21e21961c9fa9ac69f2de74
imarin309 <16imarin309@gmail.com>
2026-02-28 18:01:11 +0900

    modify footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,7 +2,7 @@ export function Footer() {
   return (
     <footer className="mt-20 border-t border-white/10 py-8">
       <div className="container mx-auto px-6 text-center text-gray-400">
-        <p>&copy; 2026 Portfolio. All rights reserved.</p>
+        <p>&copy; 2026 seki-saki.com All rights reserved.</p>
       </div>
     </footer>
   );
